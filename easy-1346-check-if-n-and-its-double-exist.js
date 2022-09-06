f7e3ba8e37997ce7272c06fc854fd7572fef771d
Paul Cabezal <paulcabezal@gmail.com>
2022-09-06 11:56:35 -0400

    2 easy

---
+++ easy-1346-check-if-n-and-its-double-exist.js
@@ -0,0 +1,15 @@
+var checkIfExist = function(arr) {
+    const hashTable = {};
+    
+    for(let i = 0; i < arr.length; i ++) {
+      const currValue = arr[i];
+      
+      if(hashTable[currValue] !== undefined) {
+        return true
+      }
+      hashTable[currValue / 2] = 'poo';
+      hashTable[currValue * 2] = 'poo';
+    }
+  
+  return false;
+};
--- easy-26-remove-duplicates-from-sorted-array.js
+++ easy-26-remove-duplicates-from-sorted-array.js
@@ -2,13 +2,8 @@ var removeDuplicates = function(nums) {
     let pointer = 1;
     for (let i = 0; i < nums.length - 1; i++) {
         if (nums[i] !== nums[i+1]) {
-            pointer++;
-            nums[pointer] = nums[i + 1];
+            nums[pointer++] = nums[i + 1];
         } 
-
+    };
     return pointer;
-};
-
-console.log(removeDuplicates([1,1,2]));
-
-// [0,0,1,1,1,2,2,3,3,4]
+}
