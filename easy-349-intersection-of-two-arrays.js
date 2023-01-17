054e8bad8080e5a659ebe0cd5a02e6950c74254b
paulcabezal@gmail.com <paulcabezal@gmail.com>
2023-01-17 11:07:46 -0500

    1 easy

--- easy-349-intersection-of-two-arrays.js
+++ easy-349-intersection-of-two-arrays.js
@@ -4,20 +4,11 @@ var intersection = function(nums1, nums2) {
 
   for (let i = 0; i < nums1.length; i++) {
     map[nums1[i]] = 1
-    
   }
 
   for (let i = 0; i < nums2.length; i++) {
     if (map[nums2[i]]) answer.add(nums2[i])
-    
   }
-
-
-console.log(map);
-  console.log('map ' + map)
-  console.log('answer' + answer)
 
   return Array.from(answer)
 };
-
-console.log(intersection([1,2,2,1], [2,2]));
