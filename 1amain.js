4233308d4c2b859af4e5d307d402bb1e9e36f604
paulcabezal@gmail.com <paulcabezal@gmail.com>
2023-01-25 13:55:53 -0500

    1 easy

--- 1amain.js
+++ 1amain.js
@@ -1,13 +1,47 @@
-var firstUniqChar = function(s) {
-  let map = {}
-  for (let i = 0; i < s.length; i++) {
-    map[s[i]] ? map[s[i]]++ : map[s[i]] = 1
-  }
-  for (let i = 0; i < s.length; i++) {
-    if (map[s[i]] == 1) return i
-  }
-  return -1
-}
+// /**
+//  * @param {number[]} nums1
+//  * @param {number[]} nums2
+//  * @return {number[]}
+//  */
+// var intersect = function(nums1, nums2) {
+//     let map = {}
+//     let answer = []
 
-console.log(firstUniqChar('loveleetcode'));
-console.log(firstUniqChar('aabb'));
+//     nums1.forEach((e, i) => {
+//       map[i] = e
+//     })
+
+//     console.log(map);
+
+//     nums2.forEach((e,i) => {
+//       console.log(map[i], e);
+//       if (map[i] == e) answer.push(e)
+//     })
+
+//     return answer
+// };
+
+var intersect = function(nums1, nums2) {
+  obj = {};
+  result = [];
+  for(let i of nums1){
+    console.log('i', i);
+      obj[i] = obj[i] ? obj[i]+1 : 1
+      console.log('obj i', obj[i]);
+  }
+
+
+  console.log(obj);
+  for(let i of nums2){
+      if(obj[i]){
+          obj[i]--
+          result.push(i)
+      }
+  }
+  console.log(obj);
+  return result
+};
+
+
+console.log(intersect([1,2], [2,2]));
+// console.log(intersect( [9,4,9,8,4], [4,9,5]));
---
+++ easy-350-intersection-of-two-arrays-ii.js
@@ -0,0 +1,17 @@
+
+var intersect = function(nums1, nums2) {
+  obj = {};
+  result = [];
+  for(let i of nums1){
+      obj[i] = obj[i] ? obj[i]+1 : 1
+  }
+
+  for(let i of nums2){
+      if(obj[i]){
+          obj[i]--
+          result.push(i)
+      }
+  }
+  return result
+};
+
